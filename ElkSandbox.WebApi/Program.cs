2b119cab727201517c548e09926de597fdc4cab6
Behnam <behnam.dev@outlook.com>
2025-04-13 22:46:08 +0330

    Set up some logs

--- ElkSandbox.WebApi/Program.cs
+++ ElkSandbox.WebApi/Program.cs
@@ -1,10 +1,29 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"ElkSandbox-logs-{DateTime.UtcNow:yyyy.MM.dd}"
+    })
+    .CreateLogger();
+
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,9 +38,10 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weather-forecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    app.Logger.LogInformation("Reporting Weather");
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -32,6 +52,15 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+
+app.MapGet("/say-hello", (string name) =>
+{
+    app.Logger.LogInformation("Saying Hello to {name}", name);
+    return $"Hello {name}";
+})
+.WithName("SayHello");
+
 
 app.Run();
 
